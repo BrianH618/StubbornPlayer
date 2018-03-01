95773e0265100cb4566fc263b50ccd4c78df83dc
brianh618 <bherrera10@mail.csuchico.edu>
2018-03-01 11:43:45 -0800

    Asks how many times you wish to play and how many they won

--- main.cpp
+++ main.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 int main()
 {
+  int NumberOfRounds;
+  int GamesWon = 0;
+
   SecretDoor game;
+  cout<<"How many times do you wish to play?\n";
+  cin>>NumberOfRounds;
+  for(int Counter =0;Counter < NumberOfRounds; Counter++)
+  {
+    game.newGame();
     
+    game.guessDoorC();
+    
+    game.guessDoorC();
+    
+    if(game.isWinner() == true)
+    {
+        GamesWon ++;
+    }
+
+
+  }
+  cout<<"You won " <<GamesWon<<" times\n";
+
   return 0;
 }
